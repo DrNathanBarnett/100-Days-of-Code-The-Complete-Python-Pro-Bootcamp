ee4356303d52563dc8af769831d35ec92769d121
Nathan James Barnett <nathan@netadiscovery.org>
2025-07-25 13:22:24 +1000

    Add files via upload

---
+++ Section 4: Day 4 - Beginner - Randomisation and Python Lists/Heads or tails random module practice.py
@@ -0,0 +1,10 @@
+#come up with a script that represents heads or tails.
+
+import random
+random_number = random.randint(0,1)
+
+if random_number == 0:
+    print("Heads")
+else:
+    print("Tails")
+
