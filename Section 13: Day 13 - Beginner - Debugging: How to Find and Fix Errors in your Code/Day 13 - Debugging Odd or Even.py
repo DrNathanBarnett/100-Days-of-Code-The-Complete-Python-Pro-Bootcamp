ec7e1aa21564d9d666815928e20f94faba0cc181
Nathan James Barnett <nathan@netadiscovery.org>
2025-08-08 18:54:56 -0700

    Add files via upload

---
+++ Section 13: Day 13 - Beginner - Debugging: How to Find and Fix Errors in your Code/Day 13 - Debugging Odd or Even.py
@@ -0,0 +1,14 @@
+def odd_or_even(number):
+    if number % 2 = 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+
+
+#The above code was corrected to be the following
+
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
